e10b8e535efe95af793950e28aacd4f80e15bc2d
Bren Murrell <bren@moon.co.nz>
2021-06-04 10:34:36 +1200

    add bren to seeds

--- server/db/seeds/03_users.js
+++ server/db/seeds/03_users.js
@@ -7,7 +7,15 @@ exports.seed = (knex) => {
       return knex('users').insert([
         { id: '1', display_name: 'brentest', photo_url: '' },
         { id: '2', display_name: 'timmy-tester', photo_url: '' },
-        { id: '3', display_name: 'davidg', photo_url: '' }
+        { id: '3', display_name: 'davidg', photo_url: '' },
+        {
+          id: 'brenmurrell',
+          display_name: 'Bren Murrell',
+          photo_url: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t1.6435-1/c89.195.1304.1304a/s320x320/158347101_10161040210886030_4581579484502990198_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=0c64ff&_nc_ohc=FP3rwNWmK20AX9ihVkO&_nc_ht=scontent-hkt1-1.xx&tp=28&oh=798b3a8a75cd8e831b9b85c1789cd531&oe=60DF6326',
+          spotify_url: 'https://open.spotify.com/user/brenmurrell',
+          spotify_api_url: 'https://api.spotify.com/v1/users/brenmurrell',
+          email: 'spotify@moon.co.nz'
+        }
       ])
     })
 }
